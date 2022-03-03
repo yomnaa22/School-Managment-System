792ecd52984460857ecfa3c3a75e2dd52834e6c5
yomna <yuumnaa59@gmail.com>
2022-03-03 17:28:19 +0200

    re

--- database/migrations/2022_02_19_144456_create_course_student_table.php
+++ database/migrations/2022_02_19_144456_create_course_student_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCoursesStudentsTable extends Migration
+class CreateCourseStudentTable extends Migration
 {
     /**
      * Run the migrations.
