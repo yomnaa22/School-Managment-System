e26ce2512c37ea221869e46a331971099e76e5c5
yomna <yuumnaa59@gmail.com>
2022-03-10 21:36:30 +0200

    middleware

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('messages', [chatController::class, 'message']);
 //login Admin
-Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('login', 'App\Http\Controllers\AuthController@login')->middleware(['cors']);
 Route::post('register', 'App\Http\Controllers\AuthController@register');
 
 
