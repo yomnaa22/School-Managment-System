60d4f4b09fff81f693866ae7230cb553a226dfa6
yomna <yuumnaa59@gmail.com>
2022-03-10 18:40:18 +0200

    handle

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,10 @@ class Cors
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        return $next($request)->header("Access-Control-Allow-Origin", '*')
+        ->header("Access-Control-Allow-Credentials", true)
+        ->header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        ->header("Access-Control-Allow-Headers", 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 
     }
 }
