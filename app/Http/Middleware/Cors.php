110ec750318d5234754585e1765c34be3609d968
yomna <yuumnaa59@gmail.com>
2022-03-10 20:39:31 +0200

    updtt

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,26 +16,11 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-    //     if( app.get("env") === "production"  ) 
-    //     {
        
-    //        sess.cookie.secure = true;
-    //        app.set("trust proxy", 1);
-    //    }
-//     if (r.Method == "OPTIONS") {
-//         w.WriteHeader(http.StatusOK);
-       
-//  return $next($request)->header("Access-Control-Allow-Origin", '*')
-//  ->header("Access-Control-Allow-Credentials", true)
-//  ->header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-//  ->header("Access-Control-Allow-Headers", 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-//     }z
+        return $next($request)->header("Access-Control-Allow-Origin", '*')
+        ->header("Access-Control-Allow-Credentials", true)
+        ->header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        ->header("Access-Control-Allow-Headers", 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 
-header.Add("Access-Control-Allow-Origin", "*");
-header.Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-header.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-
-
-
-}
+    }
 }
